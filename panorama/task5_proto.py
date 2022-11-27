e3c5c1ebd8df131cbff16001335121dde7fa6b3b
Jan Wawszczak <janekw23@gmail.com>
2022-11-27 23:04:03 +0100

    Fix typo

--- panorama/task5_proto.py
+++ panorama/task5_proto.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def compute_blending_weigths(image: np.ndarray) -> np.ndarray:
+def compute_blending_weights(image: np.ndarray) -> np.ndarray:
     xs, ys = np.meshgrid(np.arange(image.shape[0]), np.arange(image.shape[1]))
     xs, ys = xs.astype(float), ys.astype(float)
     
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     import cv2
     
     img = cv2.imread('undistorted.png')
-    weights = compute_blending_weigths(img[:300])
+    weights = compute_blending_weights(img[:300])
     print(weights.min(), weights.max())
 
     cv2.imshow('weights', weights)
