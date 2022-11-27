96f44d88270d9ea6c771ab1bd2946588e5ec7f5c
Jan Wawszczak <janekw23@gmail.com>
2022-11-27 23:04:32 +0100

    Complete task 5 prototype

--- panorama/task4_proto.py
+++ panorama/task4_proto.py
@@ -1,13 +1,31 @@
 from task3_proto import find_homography
 from task2_proto import transform_image, compute_bounds_and_offset
+from task5_proto import compute_blending_weights
+
+from typing import Tuple
 
 import numpy as np
 import cv2
 
+def expand_image(target_size: Tuple[int, int], offset: np.ndarray, image: np.ndarray) -> np.ndarray:
+    shape = list(image.shape)
+    shape[:2] = target_size
+    new_image = np.zeros_like(image, shape=shape)
+
+    tl = offset
+    br = (tl[0] + image.shape[0], tl[1] + image.shape[1])
+    new_image[tl[0]:br[0], tl[1]:br[1]] = image
+
+    return new_image
 
 def stich_images(adjustable_image: np.ndarray, fixed_image: np.ndarray, homography: np.ndarray) -> np.ndarray:
     ''' Stich images into one image by applying homography to adjustable_image and blending the result with fixed_image. '''
-    transformed_image, transformed_image_offset = transform_image(adjustable_image, homography)
+
+    adjustable_image_weights = compute_blending_weights(adjustable_image)
+    fixed_image_weights = compute_blending_weights(fixed_image)
+
+    trans_image, trans_image_offset = transform_image(adjustable_image, homography)
+    trans_image_weights, _ = transform_image(adjustable_image_weights, homography)
 
     # Swap axes ordering so that we can work with images just like with matrices.
     np.swapaxes(adjustable_image, 0, 1)
@@ -15,29 +33,42 @@ def stich_images(adjustable_image: np.ndarray, fixed_image: np.ndarray, homograp
 
     # Compute new image shape and its origin offset w.r.t. fixed_image's origin.
 
-    transformed_xs = np.array([0, transformed_image.shape[0]]) + transformed_image_offset[0]
-    transformed_ys = np.array([0, transformed_image.shape[1]]) + transformed_image_offset[1]
+    trans_xs = np.array([0, trans_image.shape[0]]) + trans_image_offset[0]
+    trans_ys = np.array([0, trans_image.shape[1]]) + trans_image_offset[1]
     fixed_xs = np.array([0, fixed_image.shape[0]])
     fixed_ys = np.array([0, fixed_image.shape[1]])
 
     stiched_image_shape = list(fixed_image.shape)
     stiched_image_shape[:2], stiched_image_offset = compute_bounds_and_offset(
-        np.hstack([fixed_xs, transformed_xs]), np.hstack([fixed_ys, transformed_ys]))
+        np.hstack([fixed_xs, trans_xs]), np.hstack([fixed_ys, trans_ys]))
     stiched_image_shape = tuple(stiched_image_shape)
 
     # Copy images onto the new stiched image in their correct positions.
+    # Offsets are initially relative to fixed_image's origin and we transform them to be
+    # relative to stiched_image's origin.
 
-    stiched_image_offset = np.array(stiched_image_offset)
-    transformed_image_offset = np.array(transformed_image_offset)
-    stiched_image = np.zeros(stiched_image_shape, fixed_image.dtype)
+    fixed_image_offset = -np.array(stiched_image_offset)
+    trans_image_offset = fixed_image_offset + np.array(trans_image_offset)
     
-    tl = -stiched_image_offset + transformed_image_offset
-    br = (tl[0] + transformed_image.shape[0], tl[1] + transformed_image.shape[1])
-    stiched_image[tl[0]:br[0], tl[1]:br[1]] = transformed_image
+    # Combine images using weighted average.
 
-    tl = -stiched_image_offset
-    br = (tl[0] + fixed_image.shape[0], tl[1] + fixed_image.shape[1])
-    stiched_image[tl[0]:br[0], tl[1]:br[1]] = fixed_image
+    trans_image_full = expand_image(stiched_image_shape[:2], trans_image_offset, trans_image)
+    trans_image_weights_full = expand_image(stiched_image_shape[:2], trans_image_offset, trans_image_weights)
+    fixed_image_full = expand_image(stiched_image_shape[:2], fixed_image_offset, fixed_image)
+    fixed_image_weights_full = expand_image(stiched_image_shape[:2], fixed_image_offset, fixed_image_weights)
+
+    trans_image_weights_full = np.expand_dims(trans_image_weights_full, 2)
+    fixed_image_weights_full = np.expand_dims(fixed_image_weights_full, 2)
+
+    trans_image_full = trans_image_full.astype(float) * trans_image_weights_full
+    fixed_image_full = fixed_image_full.astype(float) * fixed_image_weights_full
+
+    # Avoid zero division.
+    both_zero = (trans_image_weights_full == 0) & (fixed_image_weights_full == 0)
+    trans_image_weights_full[both_zero] = 1
+    fixed_image_weights_full[both_zero] = 1
+
+    stiched_image = np.rint((trans_image_full + fixed_image_full) / (trans_image_weights_full + fixed_image_weights_full)).astype(fixed_image.dtype)
 
     # Transform image back into image-like coordinates.
     np.swapaxes(stiched_image, 0, 1)
@@ -70,23 +101,4 @@ homography = find_homography(img2_points, img1_points)
 
 stiched_image = stich_images(img2, img1, homography)
 cv2.imshow('stiched', stiched_image)
-cv2.waitKey(0)
-
-exit()
-
-
-img, origin_offset = transform_image(img2, homography)
-
-cv2.imshow('before', img2)
-cv2.imshow('after', img)
-cv2.waitKey(0)
-
-# concated = cv2.hconcat([img2, img])
-# cv2.imshow('before and after', concated)
-# cv2.waitKey(0)
-
-img_draw = img1.copy()
-nonzero = img != 0
-img_draw[nonzero] = img[nonzero]
-cv2.imshow('overlapped', img_draw)
 cv2.waitKey(0)
